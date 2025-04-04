535b5eba7d06922e7091386304c1c590b7deb072
shamilemir <shamilcanbolat@gmail.com>
2025-04-03 21:57:17 -0400

    added example test

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/ExampleTest.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+
+    @Test
+    public void sampleTestScenario() {
+        assertEquals(1+1,2);
+    }
+
+}
